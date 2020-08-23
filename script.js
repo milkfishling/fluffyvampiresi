d8da3cfc8791613f428d6e7d5a786c35d93f2875
milkfishling <milkfishling@users.noreply.github.com>
2020-08-23 16:01:30 -0400

    naming bobs and stuff

--- script.js
+++ script.js
@@ -15,19 +15,24 @@ class Ball{
   }
   movement(){
     let tx = random(-this.speed, this.speed);
-    while(this.x + tx >= WIDTH || this.x + tx <= 0) tx = random(-this.speed, this.speed);
+    while(this.x + tx >= WIDTH || this.x + tx <= 0)
+      tx = random(-this.speed, this.speed);
     this.x += tx;
     let ty = random(-this.speed, this.speed);
-    while(this.y + ty >= HEIGHT || this.y + ty <= 0) ty = random(-this.speed, this.speed);
+    while(this.y + ty >= HEIGHT || this.y + ty <= 0)
+      ty = random(-this.speed, this.speed);
     this.y += ty;
+
     // console.log(tx, ' ', ty, '\n');
   }
 }
 
 var ballfamily = [];
-
+var names = [];
 // phill = new Ball(350, 300, 10, 20);
 // bill = new Ball(355, 300, 20, 20);
+
+var qtt = 50;
 
 function setup(){
   createCanvas(WIDTH, HEIGHT);
@@ -35,9 +40,16 @@ function setup(){
   noStroke();
   fill(200, 182, 106);
   let counter;
-  for(counter = 0; counter < 10; counter++){
+  for(counter = 0; counter < qtt; counter++){
     let colour = color(random(255), random(255), random(255));
-    ballfamily.push(new Ball(350, 300, 10, 20, "bob", colour));
+    ballfamily.push(new Ball(350, 300, random(5, 20), random(2, 20), "bob", colour));
+  }
+}
+
+function mousePressed(){
+  for(counter = 0; counter < qtt; counter++){
+    let colour = color(random(255), random(255), random(255));
+    ballfamily[counter] = new Ball(mouseX, mouseY, random(5, 20), random(2, 20), "bob", colour);
   }
 }
 
@@ -47,8 +59,10 @@ function draw(){
   // phill.movement();
   // bill.display();
   // bill.movement();
-  for(counter = 0; counter < 10; counter++){
+  for(counter = 0; counter < qtt; counter++){
     ballfamily[counter].display();
     ballfamily[counter].movement();
+    if(pow(mouseX - ballfamily[counter].x, 2) + pow(mouseY - ballfamily[counter].y, 2) <= pow(ballfamily[counter].s, 2))
+      console.log(ballfamily[counter].name);
   }
 }
